7399013cb1c1845597550d84ae2136fb44640134
DylanC <comeaudc@comcast.net>
2025-10-09 12:02:01 -0400

    Added Conditional Nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,31 +1,39 @@
 import style from "./Nav.module.css";
 import { Link } from "react-router-dom";
 import { useAuth } from "../../context/authContext/authContext";
+import { useUser } from "../../context/userContext/userContext";
 
 export default function Nav() {
+  const { user } = useUser();
   const { logout } = useAuth();
 
   function handleLogout() {
     logout();
   }
-  
+
   return (
     <nav className={style.nav}>
       <Link to={"/"}>
         <h3>Home</h3>
       </Link>
-      <Link to={"/dash"}>
-        <h3>Dashboard</h3>
-      </Link>
-      <Link to={"/game"}>
-        <h3>Game</h3>
-      </Link>
-      <Link to={"/auth"}>
-        <h3>Login/SignUp</h3>
-      </Link>
-      <a>
-        <h3 onClick={handleLogout}>Logout</h3>
-      </a>
+
+      {user ? (
+        <>
+          <Link to={"/dash"}>
+            <h3>Dashboard</h3>
+          </Link>
+          <Link to={"/game"}>
+            <h3>Game</h3>
+          </Link>{" "}
+          <a>
+            <h3 onClick={handleLogout}>Logout</h3>
+          </a>
+        </>
+      ) : (
+        <Link to={"/auth"}>
+          <h3>Login/SignUp</h3>
+        </Link>
+      )}
     </nav>
   );
 }
